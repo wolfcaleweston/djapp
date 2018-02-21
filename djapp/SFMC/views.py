044a47f5f1abcb870dce6cb7bc57fd0b87117e14
Weston Wolfcale <wolfcaleweston@gmail.com>
2018-02-21 04:44:17 -0500

    changed HTML for site

--- djapp/SFMC/views.py
+++ djapp/SFMC/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'SFMC/post_list.html', {})
